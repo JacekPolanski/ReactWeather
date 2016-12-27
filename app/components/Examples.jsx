3f528508d9eb6d4e25441a042fcceecf4dfb3eb9
Jacek Polański <kontakt@jpolanski.pl>
2016-12-27 17:40:31 +0100

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,3 +1,8 @@
 var React = require('react');
 
-module.exports = () => <h3>Examples component</h3>;
+module.exports = () => (
+  <div>
+    <h3>Examples component</h3>
+    <p>Welcome to examples page!</p>
+  </div>
+);
